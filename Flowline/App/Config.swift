4d03cda8ebb8a539f8738373446c8663400390d6
danylokv <danylokv@gmail.com>
2026-03-17 11:47:48 +0100

    Chenged new AI model

--- Flowline/App/Config.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum Config {
-    static let geminiAPIKey = "AIzaSyC786SuzAELww2Ft-Hz_l2KggG7LOcR-k8"
-    static let claudeAPIKey = "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"
-
-}
-
